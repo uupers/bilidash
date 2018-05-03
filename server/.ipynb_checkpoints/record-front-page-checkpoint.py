c8ffe2d831f8ca2ad273758f34e6971dc6d3f76b
OctoMiao <emptymalei@qq.com>
2018-05-03 13:49:09 -0600

    demo the code

--- server/.ipynb_checkpoints/record-front-page-checkpoint.py
+++ server/.ipynb_checkpoints/record-front-page-checkpoint.py
@@ -11,21 +11,16 @@ import bilidash
 # Define defaults
 
 dt_now = bilidash.current_dt_str()
-datafile = dt_now + "data.csv"
+datafile = dt_now + "-data.csv"
 baseurl='https://www.bilibili.com/'
 dt_now = str(datetime.datetime.now() )
+deltat = 30
 
-# Define useful functions
-if os.path.exists(datafile):
-    append_write = 'a' # append if already exists
-else:
-    append_write = 'w' # make a new file if not
 
 
 # Scrape raw data
 html = urlopen(baseurl).read().decode('utf-8')
 soup = BeautifulSoup(html, features='lxml')
-
 
 
 # Create scheduler
@@ -35,10 +30,17 @@ s = sched.scheduler(time.time, time.sleep)
 # Run program
 def groomjob():
     
-    packdata = bilidash.groomjob(soup)
-    bilidash.DTRecorder(dt_now, packdata, datafile, append_write)
+    if os.path.exists(datafile):
+        append_write = 'a' # append if already exists
+    else:
+        append_write = 'w' # make a new file if not
 
-bilidash.permcronsched( s, 1, groomjob )
+    
+    packdata = bilidash.groomjob(soup)
+    bilidash.DTRecorder( bilidash.current_dt_str(), packdata, datafile, append_write)
+    print('Data Dumped')
+
+bilidash.permcronsched( s, 20, groomjob )
 
 s.run()
 
--- server/bilidash/cronjob.py
+++ server/bilidash/cronjob.py
@@ -4,6 +4,7 @@ def current_dt_str():
     """
     returns year-month-day-hour-min-sec
     """
+    
     dt_now = datetime.datetime.now()
     # datetime.date(dt_now.year, dt_now.month, dt_now.day)
     return str( dt_now.year ) + '-'+  str( dt_now.month ) + '-'+  str( dt_now.day ) + '-'+  str( dt_now.hour ) + '-'+  str( dt_now.minute ) + '-'+  str( dt_now.second )
